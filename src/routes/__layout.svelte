7f7670874007ca2ba0fca76cd60a2db2a90a94ba
Miko <jherjati@gmail.com>
2022-01-28 15:55:33 +0700

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Footer from "../components/footer.svelte";
+  import Header from "../components/header.svelte";
+  import "../app.css";
+</script>
+
+<div class="bg-white">
+  <div class="relative overflow-hidden">
+    <Header />
+
+    <main>
+      <slot />
+    </main>
+
+    <Footer />
+  </div>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        teal: colors.teal,
+        cyan: colors.cyan,
+        "warm-gray": colors.stone,
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
